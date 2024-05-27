94786b31254b07d7c03d726fdd2f7612586c1bb8
gatere <mercynjerigatere@gmail.com>
2024-05-27 23:53:58 +0300

    Adds lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+     evens = [n for n in num_list if n%2 == 0]
+     return evens
+
+
+    
 
 def make_exclamation(sentence_list):
-    pass
+    exclamated_sentences = (sentence + "!" for sentence in sentence_list)
+    return exclamated_sentences
+
+print (make_exclamation(["I like computers","I require coffee","Live long and prosper",]))
+print (return_evens([1,2,3,4,5,6,7,8,9,0,12]))
